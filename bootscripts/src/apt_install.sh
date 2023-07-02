436c45648c3fd7eb032b86ef5ef45b92acaae9ef
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-02 00:49:34 -0500

    Flatpak+dbeaver

--- bootscripts/src/apt_install.sh
+++ bootscripts/src/apt_install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+echo "Installing applications. apt+snap+flatpak"
 sudo add-apt-repository ppa:deadsnakes/ppa -y  # Python
 sudo add-apt-repository ppa:aslatter/ppa -y  # alacritty
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath manifest.ini) sudo apt install)
 # Snap for unstable packages that I want to be 'bleeding edge.' I might regret this.
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath snap_manifest.ini) sudo snap install --classic)
+# Flatpak for unstable packages that I want to be 'bleeding edge.' I might regret this.
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath flatpak_manifest.ini) flatpak install flathub)
