59ccc016266b09a5e40327ef16840614948f19c7
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-02 11:14:55 -0500

    Restrict snap install --classic flag to the 1 package that needs it. Try out zotero for managing citations

--- bootscripts/src/apt_install.sh
+++ bootscripts/src/apt_install.sh
@@ -4,8 +4,9 @@ echo "Installing applications. apt+snap+flatpak"
 sudo add-apt-repository ppa:deadsnakes/ppa -y  # Python
 sudo add-apt-repository ppa:aslatter/ppa -y  # alacritty
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath manifest.ini) sudo apt install)
-# Snap for unstable packages that I want to be 'bleeding edge.' I might regret this.
-(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath snap_manifest.ini) sudo snap install --classic)
+sudo snap install --classic nvim  # Idk why nvim needs elevated permissions :(
+# Trying some things in Snap. Experimental. Sandbox when we can. https://snapcraft.io/docs/snap-confinement
+(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath snap_manifest.ini) sudo snap install)
 # Flatpak for unstable packages that I want to be 'bleeding edge.' I might regret this.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath flatpak_manifest.ini) flatpak install flathub)
