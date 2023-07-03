5da55dd8f5da55c2d18e6020dbacc8dbf00cccca
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-03 11:02:26 -0500

    Add fast PCRE-enabled ripgrep dep

--- bootscripts/src/cargo_sprinkles.sh
+++ bootscripts/src/cargo_sprinkles.sh
@@ -2,5 +2,6 @@
 set -Eeuo pipefail
 
 rustup default nightly
-cargo install tokei xsv bat git-diff
+cargo install tokei xsv bat git-delta 
 cargo install --features "parquet/cli"  parquet
+cargo install --features "simd-accel,pcre2"  ripgrep
