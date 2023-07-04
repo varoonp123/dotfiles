ae1840ed0a3f543e1c08de8398670c6d0ccf1945
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-04 15:56:27 -0500

    Symlink a lua subdir to use modules when configuring nvim

--- bootscripts/src/bash_symlinks.sh
+++ bootscripts/src/bash_symlinks.sh
@@ -6,4 +6,5 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 mkdir -p ~/.config/nvim/
 ln -sf ~/dotfiles/nvim_config/vimrc.vim ~/.config/nvim/vimrc.vim
 ln -sf ~/dotfiles/nvim_config/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/nvim_config/lua/ ~/.config/nvim/lua
 ln -sf ~/dotfiles/vscode/settings.json ~/.vscode/settings.json
