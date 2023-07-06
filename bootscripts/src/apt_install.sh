b0cfaf53cd65b84b9777bf58905bb77764f2dab9
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-06 09:54:24 -0500

    Snap install was failing because the stupid install command isn't idempotent. Big sad

--- bootscripts/src/apt_install.sh
+++ bootscripts/src/apt_install.sh
@@ -6,7 +6,8 @@ sudo add-apt-repository ppa:aslatter/ppa -y  # alacritty
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath manifest.ini) sudo apt install)
 sudo snap install --classic nvim  # Idk why nvim needs elevated permissions :(
 # Trying some things in Snap. Experimental. Sandbox when we can. https://snapcraft.io/docs/snap-confinement
-(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath snap_manifest.ini) sudo snap install)
+# -L1 because https://forum.snapcraft.io/t/trying-to-re-install-multiple-packages-with-snap-install-fails-with-install-refresh-information-results-from-the-store/24859
+(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath snap_manifest.ini) -L1 sudo snap install)
 # Flatpak for unstable packages that I want to be 'bleeding edge.' I might regret this.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 (cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath flatpak_manifest.ini) flatpak install flathub)
