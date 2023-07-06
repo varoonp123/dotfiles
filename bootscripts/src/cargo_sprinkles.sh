cc970bdd7842ca19710e4a5080a00c59a311897d
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-07-06 14:51:53 -0500

    cargo install dust to quickly see disk usage

--- bootscripts/src/cargo_sprinkles.sh
+++ bootscripts/src/cargo_sprinkles.sh
@@ -2,6 +2,6 @@
 set -Eeuo pipefail
 
 rustup default nightly
-cargo install tokei xsv bat git-delta 
+cargo install tokei xsv bat git-delta du-dust
 cargo install --features "parquet/cli"  parquet
 cargo install --features "simd-accel,pcre2"  ripgrep
