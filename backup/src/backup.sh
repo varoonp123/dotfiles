2e04f62cc51b5de04a4bf42bfe1742bbc2cd4dac
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-08-18 11:19:32 -0500

    Thought I committed these backup tweaks before...

--- backup/src/backup.sh
+++ backup/src/backup.sh
@@ -4,8 +4,8 @@ set -xEeuo pipefail
 # Ignore build artifacts for JS, python, rust 
 tar -b196 --checkpoint=5000 --checkpoint-action="echo=#%u: %T" -I"xz -9 -T0" --totals -C ~/ -pcf  latest_backup.tar.xz \
         --exclude "node_modules" --exclude "venv" --exclude "target"  \
-        Downloads/ Documents/ snap/zotero-snap/common/Zotero/*.sqlite Music/ Pictures/
+        Documents/ Music/ Pictures/
 
 # More than 500 requests and we risk getting higher s3 bills.
-rclone copyto --s3-storage-class=GLACIER_IR --s3-upload-concurrency=12 --progress --s3-max-upload-parts=256 \
+rclone copyto --s3-storage-class=GLACIER_IR --s3-upload-concurrency=12 --progress --s3-max-upload-parts=256 --max-transfer=25G \
         ~/latest_backup.tar.xz "aws-s3-root:shivaji-backup/data/backup_$(date +%F).tar.xz"
