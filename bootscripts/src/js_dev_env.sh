fab288a091fcd03f4b7bfce23b87e1dfa420a9bd
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-09-13 14:16:30 -0500

    Try out Bun

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+curl -fsSL https://bun.sh/install | bash
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 nvm install 18
 nvm use 18
