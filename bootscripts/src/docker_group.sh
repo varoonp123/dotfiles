d99581f2acebbd23a3be8fe5c45674191544d615
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-10-19 12:59:09 -0500

    Move docker installation earlier in the bootscript and install from apt rather than snap. Minimize how much snap we use. Remove the special docker group since we aren't using docker from snap anymore

--- bootscripts/src/docker_group.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-# Specifically for using docker snap package. https://forum.snapcraft.io/t/snap-declaration-request-for-docker-snap/394
-# and
-# https://askubuntu.com/questions/907110/docker-snap-cannot-connect-to-the-docker-daemon-is-the-docker-daemon-running-o
-sudo groupadd -f --system docker
-sudo usermod -aG docker $USER
-newgrp docker
--- bootscripts/src/main.sh
+++ bootscripts/src/main.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuxo pipefail
 
 function execute_file(){
 	(cd ~/dotfiles/bootscripts/src/ && ./$1)
 }
 
-execute_file docker_group.sh
+execute_file docker.sh
 execute_file compilers.sh
 execute_file apt_install.sh
 execute_file bash_symlinks.sh
 
-execute_file docker.sh
 execute_file cargo_sprinkles.sh
 execute_file js_dev_env.sh
 execute_file language_servers_install.sh
-
