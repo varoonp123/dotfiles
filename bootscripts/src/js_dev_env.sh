08ffa97fc983a135c3c520b9fb058345573c9b4b
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-10-19 12:56:48 -0500

    enable nvm in the boot script without restarting the shell so that later installations work

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -4,3 +4,5 @@ curl -fsSL https://bun.sh/install | bash
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 nvm install 18
 nvm use 18
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
