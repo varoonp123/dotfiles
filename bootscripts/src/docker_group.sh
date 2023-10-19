a2cd91945782e8a09479b8042b74c9c3a595e0a3
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-10-19 12:55:43 -0500

    Idempotently add user to the docker group

--- bootscripts/src/docker_group.sh
+++ bootscripts/src/docker_group.sh
@@ -3,6 +3,6 @@ set -Eeuo pipefail
 # Specifically for using docker snap package. https://forum.snapcraft.io/t/snap-declaration-request-for-docker-snap/394
 # and
 # https://askubuntu.com/questions/907110/docker-snap-cannot-connect-to-the-docker-daemon-is-the-docker-daemon-running-o
-getent group docker || sudo addgroup --system docker
-sudo adduser $USER docker
+sudo groupadd -f --system docker
+sudo usermod -aG docker $USER
 newgrp docker
