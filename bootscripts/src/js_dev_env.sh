a5b61861901fed6da28ee7095c12c43ca0dca81f
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-10-19 15:25:16 -0500

    Put nvm env var update earlier in the script so it can be used in the same shell to install later things

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 curl -fsSL https://bun.sh/install | bash
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-nvm install 18
-nvm use 18
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 18
+nvm use 18
