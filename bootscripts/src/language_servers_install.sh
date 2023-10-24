1c3487d9fd9ffeaa06ae2a2aa461e6dfc00d470b
Varoon Pazhyanur <pazhyanurv@gmail.com>
2023-10-24 03:15:48 -0500

    Install more language servers

--- bootscripts/src/language_servers_install.sh
+++ bootscripts/src/language_servers_install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-npm i -g pyright
-npm install -g dockerfile-language-server-nodejs
-npm i -g sql-language-server
+npm i -g pyright dockerfile-language-server-nodejs sql-language-server \
+        yaml-language-server vscode-langservers-extracted vim-language-server
 
+function installJavaLanguageServer(){
+        # Eclipse java language server
+
+        local tarname="jdt-language-server-1.9.0-202203031534.tar.gz"
+        curl -OL "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.9.0/$tarname"
+        mkdir -p ~/.local/share/jdtls/
+        tar -C ~/.local/share/jdtls/ -xzf $tarname
+        ln -sf ~/.local/share/jdtls/bin/jdtls ~/.local/bin/jdtls
+        rm -rf $tarname
+
+}
+installJavaLanguageServer
