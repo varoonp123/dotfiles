2b567e978c8417e6df6897004e4bbaa8122e82f6
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-03-06 00:18:29 -0600

    Fix minor typo in dnf install script to do some xargs-file magic to properly pipe 2 manifests into the dnf install command. The previous way was incorrect

--- bootscripts/src/fedora_dnf_install.sh
+++ bootscripts/src/fedora_dnf_install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-echo "Installing applications. apt+snap+flatpak"
+echo "Installing applications. dnf+flatpak"
 sudo dnf upgrade
-
-(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath manifest.ini) -a $(realpath fedora_manifest.ini) sudo dnf install -y)
+(cd ~/dotfiles/bootscripts/src/ && xargs sudo dnf install -y < <(cat "$(realpath manifest.ini)" "$(realpath fedora_manifest.ini)"))
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-(cd ~/dotfiles/bootscripts/src/ && xargs -a $(realpath flatpak_manifest.ini) flatpak install flathub)
+(cd ~/dotfiles/bootscripts/src/ && xargs -a "$(realpath flatpak_manifest.ini)" flatpak install flathub)
