299a19b9dd14f92ec240bed943cccb6a249e301c
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-06-29 01:01:24 -0500

    Delete bun. It's nice, but a fad

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-curl -fsSL https://bun.sh/install | bash
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
