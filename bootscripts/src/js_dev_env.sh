6bf96346952f7cdfdf6cf7d5a39cda138475d516
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-10-26 13:13:33 -0400

    Upgrade to node 20

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -3,5 +3,5 @@ set -Eeuo pipefail
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 18
-nvm use 18
+nvm install 20
+nvm use 20
