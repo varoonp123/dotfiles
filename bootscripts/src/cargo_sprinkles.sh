480c9903649fe19cbcc001076972e29c6633dd1e
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-10-26 13:12:25 -0400

    Remove the ripgrep simd-accel feature because it is no longer provided with ripgrep. And it was only useful in very specific cases that I almost never ran into

--- bootscripts/src/cargo_sprinkles.sh
+++ bootscripts/src/cargo_sprinkles.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 rustup default nightly
 cargo install tokei xsv bat git-delta du-dust
 cargo install --features "parquet/cli"  parquet
-cargo install --features "simd-accel,pcre2"  ripgrep
+cargo install --features "pcre2"  ripgrep
