378b6a51692390cfc536d389a079c7b97746f147
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-10-30 17:55:33 -0400

    BUG: vimrc symlink was in an old location. I didnt notice because I dont use vim that much anymore.

--- bootscripts/src/bash_symlinks.sh
+++ bootscripts/src/bash_symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/nvim_config/vimrc.vim ~/.vimrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 mkdir -p ~/.config/nvim/
 ln -sf ~/dotfiles/nvim_config/vimrc.vim ~/.config/nvim/vimrc.vim
