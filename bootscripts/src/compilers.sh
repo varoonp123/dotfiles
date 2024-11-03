cf22568e702fb8c03859f042d3490cb2e0f92dc7
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-11-03 02:43:26 -0500

    Skip cargo install if it is already installed

--- bootscripts/src/compilers.sh
+++ bootscripts/src/compilers.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-curl https://sh.rustup.rs -sSf | sh
+if [ -x "$(command -v cargo)" ]; then
+    echo "cargo is already installed. Skipping installation"
+else
+    echo "Cargo not found. Installing"
+    curl https://sh.rustup.rs -sSf | sh
+fi
+
