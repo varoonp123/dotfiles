35f5e425c3e2a218381aeddc26ec17a9bcb8b4e3
Varoon Pazhyanur <pazhyanurv@gmail.com>
2025-07-02 02:32:19 -0400

    Upgrade to node 22 and make that the default version

--- bootscripts/src/js_dev_env.sh
+++ bootscripts/src/js_dev_env.sh
@@ -3,5 +3,6 @@ set -Eeuo pipefail
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 20
-nvm use 20
+nvm install 22
+nvm use 22
+nvm alias default 22
